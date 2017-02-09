c393dd7123cff31f1042699fdf6c226d08d5c059
AnAndrey <an.andrey.g@gmail.com>
2017-02-09 18:56:38 +0300

    'Executer' has changed location

---
+++ Executer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Executer
+{
+    class Program
+    {
+        private static string path = @"C:\Projects\Web\orolia\SampleFiles";
+        static void Main(string[] args)
+        {
+            var files = Directory.GetFiles(path);
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+                //foreach (var data in ParceFile(file))
+                //{
+                //    Console.WriteLine(data.Mark + " " + data.Value);
+                //}
+                
+
+            }
+        }
+
+ 
+    }
+}
