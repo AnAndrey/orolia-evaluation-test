1f52d49fcd0fd8f6c3394db89a92e2ee46ce199e
AnAndrey <an.andrey.g@gmail.com>
2017-02-14 20:58:03 +0300

    Major FIX: Row selection was changed on cell selection to avoid file locking in case of deleting. Deletiion and file data representation occurred at the same time.

--- WebApplication/WebApplication/wwwroot/js/app-main.js
+++ WebApplication/WebApplication/wwwroot/js/app-main.js
@@ -1,6 +1,12 @@
 ﻿(function () {
     "use strict";
 
-    angular.module("app-main", ["app-main.settings", "ui.grid", "ui.grid.resizeColumns", "ui.grid.selection", "ngFileUpload"]);
+    angular.module("app-main",
+        ["app-main.settings",
+            "ui.grid",
+            "ui.grid.resizeColumns",
+            "ui.grid.selection",
+            "ngFileUpload",
+            "ui.grid.cellNav"]);
 
 })();
--- WebApplication/WebApplication/wwwroot/js/filesController.js
+++ WebApplication/WebApplication/wwwroot/js/filesController.js
@@ -5,128 +5,113 @@
     var module = angular.module("app-main");
     module.controller("filesController", ["settings", "$scope", function (settings, $scope) {
             
-            $scope.refresh = function () {
-                $.getJSON(settings.fileServiceUrl,
-                {},
-                function (json) {
-                    var data = [];
-                    $.each(json,
-                        function (i, val) { 
-                            console.log(val);
-                            data.push(
-                                { File: val, Delete: '', FileServiceUrl: settings.fileServiceUrl, DataServiceUrl: settings.dataServiceUrl });
-                        });
-
-                    $scope.gridOptions.data = data;
-                });
-            }
-            
-            var rowSelected = function (row) {
-                console.log(row.entity.File);
-                var url = row.entity.DataServiceUrl + row.entity.File;
-
-                $.getJSON(url,
-                {},
-                function (json) {
-
-                    var chart = AmCharts.makeChart("chartdiv", {
-                        "type": "serial",
-                        "theme": "none",
-                        "marginLeft": 20,
-                        "pathToImages": "http://www.amcharts.com/lib/3/images/",
-                        "dataProvider": json,
-                        "valueAxes": [{
-                            "axisAlpha": 0,
-                            "inside": true,
-                            "position": "left",
-                            "ignoreAxisWidth": true
-                        }],
-                        "graphs": [{
-                            "balloonText": "[[category]]<br><b><span style='font-size:14px;'>[[value]]</span></b>",
-                            "bullet": "round",
-                            "bulletSize": 6,
-                            "lineColor": "#d1655d",
-                            "lineThickness": 2,
-                            "negativeLineColor": "#637bb6",
-                            "type": "line",
-                            "valueField": "value"
-                        }],
-                        "chartScrollbar": {},
-                        "chartCursor": {
-                            "cursorAlpha": 0,
-                            "cursorPosition": "mouse"
-                        },
-                        "categoryField": "mark",
-                        "categoryAxis": {
-                            "parseDates": false,
-                            "minorGridAlpha": 0.1,
-                            "minorGridEnabled": true,
-                            "labelRotation": 90
-                        }
+        $scope.refresh = function () {
+            $.getJSON(settings.fileServiceUrl,
+            {},
+            function (json) {
+                var data = [];
+                $.each(json,
+                    function (i, val) { 
+                        console.log(val);
+                        data.push(
+                            { File: val, Delete: '', FileServiceUrl: settings.fileServiceUrl, DataServiceUrl: settings.dataServiceUrl });
                     });
+
+                $scope.gridOptions.data = data;
+            });
+        }
+            
+        var rowSelected = function (row) {
+            console.log(row.entity.File);
+            var url = row.entity.DataServiceUrl + row.entity.File;
+
+            $.getJSON(url,
+            {},
+            function (json) {
+
+                var chart = AmCharts.makeChart("chartdiv", {
+                    "type": "serial",
+                    "theme": "none",
+                    "marginLeft": 20,
+                    "pathToImages": "http://www.amcharts.com/lib/3/images/",
+                    "dataProvider": json,
+                    "valueAxes": [{
+                        "axisAlpha": 0,
+                        "inside": true,
+                        "position": "left",
+                        "ignoreAxisWidth": true
+                    }],
+                    "graphs": [{
+                        "balloonText": "[[category]]<br><b><span style='font-size:14px;'>[[value]]</span></b>",
+                        "bullet": "round",
+                        "bulletSize": 6,
+                        "lineColor": "#d1655d",
+                        "lineThickness": 2,
+                        "negativeLineColor": "#637bb6",
+                        "type": "line",
+                        "valueField": "value"
+                    }],
+                    "chartScrollbar": {},
+                    "chartCursor": {
+                        "cursorAlpha": 0,
+                        "cursorPosition": "mouse"
+                    },
+                    "categoryField": "mark",
+                    "categoryAxis": {
+                        "parseDates": false,
+                        "minorGridAlpha": 0.1,
+                        "minorGridEnabled": true,
+                        "labelRotation": 90
+                    }
                 });
+            });
 
-            }
-
-        $scope.setings = settings;
+        }
 
         $scope.gridOptions = {
-                // comment the next line to enable column reordering/moving feature
-                enableColumnMenus: false,
-                enableColumnResizing: true,
-                enableRowSelection: true, 
-                enableRowHeaderSelection: false,
-                onRegisterApi: function (gridApi) {
-                    $scope.gridApi = gridApi; gridApi.selection.on.rowSelectionChanged($scope, function (row) {
-                        console.log(row.entity.File);
-                        rowSelected(row);
-                    });
-                },
-                multiSelect: false,
-                odifierKeysToMultiSelect :false,
-                noUnselect: true
-            };
+            enableColumnMenus: false,
+            enableColumnResizing: true,
+            enableRowSelection: true, 
+            enableRowHeaderSelection: false,
+            onRegisterApi: function (gridApi) {
+                $scope.gridApi = gridApi;
+                gridApi.cellNav.on.navigate($scope, function (newRowCol, oldRowCol) {
+                    rowSelected(newRowCol.row);
+                });
+            },
+            multiSelect: false,
+            odifierKeysToMultiSelect :false,
+            noUnselect: true
+        };
 
-            $scope.deleteRow = function (row) {
+        $scope.deleteRow = function (row) {
+            if (confirm("Are you sure you want to delete the '" + row.entity.File + "'?")) {
+                var param = encodeURIComponent(row.entity.File);
+                $.ajax({
+                    url: settings.fileServiceUrl + param,
+                    type: 'DELETE',
+                    success: function (result) {
+                        var index = $scope.gridOptions.data.indexOf(row.entity);
+                        $scope.gridOptions.data.splice(index, 1);
+                        $scope.gridApi.core.refresh();
+                        alertify.success("Record is deleted.");
+                    },
+                    error: function (xhr, ajaxOptions, thrownError) {
+                        alertify.error("Operation is failed. ");;
+                    }
+                });
+            }
+            else {alertify.error("Operation is cancelled.");}
+        };
 
-                if (confirm("Are you sure you want to delete the '" + row.entity.File + "'?")) {
-
-                    var param = encodeURIComponent(row.entity.File);
-                    $.ajax({
-                        url: settings.fileServiceUrl + param,
-                        type: 'DELETE',
-                        success: function (result) {
-                            var index = $scope.gridOptions.data.indexOf(row.entity);
-                            $scope.gridOptions.data.splice(index, 1);
-                            $scope.gridApi.core.refresh();
-                            alertify.success("Record is deleted.");
-                        },
-                        error: function (xhr, ajaxOptions, thrownError) {
-                            alertify.error("Operation is failed. ");;
-                        }
-                    });
-
-
-                }
-                else {
-                    alertify.error("Operation is cancelled.");
-                }
-                // });
-            };
-
-            $scope.gridOptions.columnDefs = [{
-                name: 'File'
-            }, {
+        $scope.gridOptions.columnDefs = [{ name: 'File' },
+            {
                 name: 'Delete',
                 cellTemplate: '<button class="btn primary" ng-click="grid.appScope.deleteRow(row)">Delete</button>',
-                width: 67
+                width: 67,
+                allowCellFocus: false
             }];
-
-
-
-            console.log(settings.fileServiceUrl);
-            $scope.refresh();
-
+        $scope.refresh();
     }]);
 
 })();
